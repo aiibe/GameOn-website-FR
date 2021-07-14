9482336542b41641fbe6d53e4d3d3d33425a3d17
souk <souksavanh.sy@gmail.com>
2021-07-14 12:49:45 +0200

    Refactor close/open modal

--- starterOnly/js/main.js
+++ starterOnly/js/main.js
@@ -1,7 +1,8 @@
+// Nav Menu
 const menu = document.querySelector(".nav__menu");
 const menuToggler = document.querySelector(".nav__menu-toggle");
 
-// Show/Hide Menu on click
+// Show/Hide nav menu on click
 menuToggler.addEventListener("click", () => {
   let defaultClass = "nav__menu";
   menu.className =
@@ -9,3 +10,17 @@ menuToggler.addEventListener("click", () => {
       ? `${defaultClass} ${defaultClass}--show` // Show menu
       : defaultClass; // Hide menu default
 });
+
+// MODAL
+const signupButton = document.querySelector(".hero__signup-button");
+const modal = document.querySelector(".modal");
+const modalClose = document.querySelector(".modal__close");
+
+// Open modal form
+signupButton.addEventListener(
+  "click",
+  () => (modal.className = "modal modal--open")
+);
+
+// Close modal form
+modalClose.addEventListener("click", () => (modal.className = "modal"));
